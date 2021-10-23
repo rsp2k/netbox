a9715002a21729d83a4fd2c5c1df3f32427a2f8a
rsp2k <rpm@malloys.us>
2021-10-23 03:49:24 -0600

    Consistent naming

--- netbox/dcim/choices.py
+++ netbox/dcim/choices.py
@@ -940,13 +940,13 @@ class InterfaceModeChoices(ChoiceSet):
     MODE_ACCESS = 'access'
     MODE_TAGGED = 'tagged'
     MODE_TAGGED_ALL = 'tagged-all'
-    MODE_Q_IN_Q = 'q-in-q'
+    MODE_DOT1Q_TUNNEL = 'dot1q-tunnel'
 
     CHOICES = (
         (MODE_ACCESS, 'Access'),
         (MODE_TAGGED, 'Tagged'),
         (MODE_TAGGED_ALL, 'Tagged (All)'),
-        (MODE_Q_IN_Q, '802.1ad (QinQ)'),
+        (MODE_DOT1Q_TUNNEL, '802.1ad (QinQ)'),
     )
 
 
